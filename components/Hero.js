5ac5a48d47d9bbd929aaa5846f6ee33069fe95ae
Scar2345 <145857731+Scar2345@users.noreply.github.com>
2024-10-05 00:59:31 +0530

    edited the content

--- components/Hero.js
+++ components/Hero.js
@@ -6,11 +6,11 @@ const fugaz = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
 export default function Hero() {
   return (
     <div className='py-4 md:py-10 flex flex-col gap-006 sm:gap-8'>
-      <h1 className={' spacing text-5xl sm:text-6xl md:text-7xl text-center lg:text-7xl ' + fugaz.className}>
-        <span className='textGradient'>Broodl  </span>helps you track your  <span className='textGradient'>daily  </span>mood!
+      <h1 className={'text-5xl sm:text-6xl md:text-7xl text-center lg:text-7xl ' + fugaz.className}>
+        <span className='textGradient'>Sink </span>connects you with people through instant <span className='textGradient'> messaging   </span>
       </h1>
-      <p className='spacing subtext text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[600px]'>
-      Create your mood record and see how you feel on <span className='font-semibold'>every day of every year.</span>
+      <p className='subtext text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w-[490px]'>
+      Choose your vibe, send a message to a random someone.
       </p>
       <div className='grid grid-cols-2 gap-4 w-fit mx-auto'>
         <Button text="Sign up"/>
